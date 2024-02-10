8688420eb377880b62593bb39ab9c4e31c478ec4
Will Cygan <wcygan.io@gmail.com>
2024-02-10 14:55:58 -0600

    connect to protobuf generated files

--- beam-job/app/src/main/java/io/counter/beam/App.java
+++ beam-job/app/src/main/java/io/counter/beam/App.java
@@ -1,11 +1,16 @@
 package io.counter.beam;
 
+import com.rng.v1.Packet;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+        Packet packet = Packet.newBuilder()
+                .setNumber(123)
+                .build();
         System.out.println(new App().getGreeting());
     }
 }
