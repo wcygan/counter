04649d7d0b233ece1cc8ad4b0d428a37acf53819
Will Cygan <wcygan.io@gmail.com>
2024-02-12 01:09:34 -0600

    Consuming from Kafka works :)

--- filter/src/main/java/io/counter/filter/App.java
+++ filter/src/main/java/io/counter/filter/App.java
@@ -1,16 +1,15 @@
 package io.counter.filter;
 
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.kafka.KafkaIO;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.StreamingOptions;
-import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.TypeDescriptors;
-
-import java.util.Arrays;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class App {
     public interface Options extends StreamingOptions {
@@ -21,20 +20,25 @@ public class App {
         void setInputText(String value);
     }
 
-    public static PCollection<String> buildPipeline(Pipeline pipeline, String inputText) {
+    public static PCollection<String> buildPipeline(Pipeline pipeline) {
         return pipeline
-                .apply("Create elements", Create.of(Arrays.asList("Hello", "World!", inputText)))
-                .apply("Print elements",
-                        MapElements.into(TypeDescriptors.strings()).via(x -> {
-                            System.out.println(x);
-                            return x;
+                .apply("Read from Kafka", KafkaIO.<String, String>read()
+                        .withBootstrapServers("kafka.default.svc.cluster.local:9092")
+                        .withTopic("packet")
+                        .withKeyDeserializer(StringDeserializer.class)
+                        .withValueDeserializer(StringDeserializer.class)
+                        .withoutMetadata())
+                .apply("Print records", MapElements.into(TypeDescriptors.strings())
+                        .via(record -> {
+                            System.out.println(record.getValue());
+                            return record.getValue();
                         }));
     }
 
     public static void main(String[] args) {
         var options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
         var pipeline = Pipeline.create(options);
-        App.buildPipeline(pipeline, options.getInputText());
+        App.buildPipeline(pipeline);
         pipeline.run().waitUntilFinish();
     }
 }
